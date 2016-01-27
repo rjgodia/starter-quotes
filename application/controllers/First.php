216c72b8e6d1ff973ecb6150de0baece9e8ba954
TrevorBroderick <Broderick.Trevor@gmail.com>
2016-01-27 14:13:46 -0800

    Fixed link 5, Updated First.php controller, Updated routes.php

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -15,7 +15,7 @@ class First extends Application {
     }
 
     /**
-     * Displays the first quote.
+     * Displays the first (first) quote.
      */
     function index() {
         // Load the justone as the content
@@ -39,6 +39,24 @@ class First extends Application {
 
         // Retrieve the first row
         $record  = $this->quotes->first();
+        // Merge the associative record array with the data array
+        $this->data = array_merge($this->data, $record);
+
+        // Render the page
+        $this->render();
+    }
+
+    /**
+     * Displays the quote with the specified $id.
+     *
+     * @param $id int id of the row to display.
+     */
+    function gimme($id) {
+        // Load the justone as the content
+        $this->data['pagebody'] = 'justone';
+
+        // Retrieve the row with id of $id
+        $record  = $this->quotes->get($id);
         // Merge the associative record array with the data array
         $this->data = array_merge($this->data, $record);
 
