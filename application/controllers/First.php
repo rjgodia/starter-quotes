4bae0dfe5d7104e190f3d2c07ae1a07851f32c82
kaniseo <dannyseo517@gmail.com>
2016-02-02 17:20:31 -0800

    Update First.php

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -4,24 +4,24 @@ class First extends Application {
     
 	//constructor
 	function __construct() {
-        parent::__construct();
-    }
+        	parent::__construct();
+    	}
     
 	//index that creates link to the first in the nav
 	function index() {
-        $this->data['pagebody'] = 'justone';
-        $record  = $this->quotes->first();
-        $this->data = array_merge($this->data, $record);
-        $this->render();
-    }
+        	$this->data['pagebody'] = 'justone';
+        	$record  = $this->quotes->first();
+        	$this->data = array_merge($this->data, $record);
+        	$this->render();
+    	}
 	
 	//creates zzz link to the first one in the list
 	function zzz() {
-        $this->data['pagebody'] = 'justone';
-        $record  = $this->quotes->first();
-        $this->data = array_merge($this->data, $record);
-        $this->render();
-    }
+        	$this->data['pagebody'] = 'justone';
+        	$record  = $this->quotes->first();
+        	$this->data = array_merge($this->data, $record);
+        	$this->render();
+    	}
 }
 
 ?>
