9c1b66e487bcaa107b18ae90d615e313f8552a2f
kaniseo <dannyseo517@gmail.com>
2016-02-02 16:23:04 -0800

    Added First and last link

---
+++ application/controllers/First.php
@@ -0,0 +1,15 @@
+<?php
+
+class First extends Application {
+    function __construct() {
+        parent::__construct();
+    }
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $record  = $this->quotes->first();
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+    }
+}
+
+?>
