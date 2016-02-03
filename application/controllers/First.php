142c1b0fcb7956e6456a81274c06a660195722d8
kaniseo <dannyseo517@gmail.com>
2016-02-02 17:20:01 -0800

    added comments for the first

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -1,15 +1,21 @@
 <?php
 
 class First extends Application {
-    function __construct() {
+    
+	//constructor
+	function __construct() {
         parent::__construct();
     }
-    function index() {
+    
+	//index that creates link to the first in the nav
+	function index() {
         $this->data['pagebody'] = 'justone';
         $record  = $this->quotes->first();
         $this->data = array_merge($this->data, $record);
         $this->render();
     }
+	
+	//creates zzz link to the first one in the list
 	function zzz() {
         $this->data['pagebody'] = 'justone';
         $record  = $this->quotes->first();
